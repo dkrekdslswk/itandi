0cf8221ff82bdecfab13ddb4fec5c5a9dbd72d66
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-10-29 13:07:14 +0900

    Revert "課題"
    This reverts commit 6b1c6872ba8671cb1f08d203151da38a78bfb031.

--- main.rb
+++ main.rb
@@ -5,46 +5,6 @@ class String
   end
 end
 
-#Drink Class
-class Drink
-  def initialize(argName, argPrice, argStock, argMake)
-    @name = argName
-    @price = argPrice
-    @stock = argStock
-    @make = argMake
-  end
-
-  def getName
-    return @name
-  end
-  
-  def getPrice
-    return @price
-  end
-  
-  def getStock
-    return @stock
-  end
-  
-  def updateStock(argCount)
-    @stock = @stock + argCount
-  end
-  
-  def getMake
-    return @make
-  end
-  
-  def getBuyningState(argMoney)
-    if @stock == 0
-      return '無い'
-    elsif @price <= argMoney
-      return '可能'
-    else
-      return '有る'
-    end
-  end
-end
-
 #自販機Class
 class Dihanki
 
@@ -54,79 +14,80 @@ class Dihanki
     @money = 0
     #自販機の売上金額
     @sales = 0
-    #ドリンクの情報[ドリンクの番号]
-    @drinkList = [Drink.new('コーラ', 120, 5, 'kaisya'),
-                  Drink.new('レッドブル', 200, 5, 'kaisya'),
-                  Drink.new('水', 100, 5, 'kaisya'),
-                  Drink.new('kakao', 100, 5, 'kaisya')]
+    #ドリンクの情報[ドリンクの番号][0：名、1：値段、2：在庫、[3：状態]]
+    @drink_list = [['コーラ', 120, 5,],
+                    ['レッドブル', 200, 5,],
+                    ['水', 100, 5,]]
   end
 
   #return 現在のお客様が投入したお金
-  def getMoney()
+  def viewMoney()
     return @money
   end
   
   #お金の投入
   #10,50,100,500,1000以外のが投入された場合は、投入金額に加算せず、それをそのまま釣り銭としてユーザに出力する。
-  def putMoney(argMoney)
-    case argMoney
-      when 10
-        @money += argMoney
-      when 50
-        @money += argMoney
-      when 100
-        @money += argMoney
-      when 500
-        @money += argMoney
-      when 1000
-        @money += argMoney
+  def putMoney(arg_money)
+    case arg_money
+      when 10, 50, 100, 500, 1000
+        @money += arg_money
     else 
-      refundMoney(argMoney)
+      refundMoney(arg_money)
     end
   end
 
   #自販機のジュースの情報を出す。
-  def getDrinkList()
-    return @drinkList
+  def viewDrinkList()
+    @drink_list.each() do |drink|
+      drink[3] = ''
+      if drink[2] == 0
+        drink[3] = '無い'
+      elsif drink[1] <= self.viewMoney()
+        drink[3] = '可能'
+      else
+        drink[3] = '有る'
+      end
+    end
+    return @drink_list
   end
   
   #購入できるドリンクリストのドリンク番号を出す。
-  def getInhabitableDrinkList()
-    drinkList = self.getDrinkList()
-    inhabitableDrinkList = []
-    saveP = 0
-    drinkNumber = 0
-    drinkList.each() do |drink|
-      if drink.getBuyningState(@money) == '可能'
-        inhabitableDrinkList[saveP] = drinkNumber
-        saveP += 1
+  def viewInhabitableDrinkList()
+    drink_list = self.viewDrinkList()
+    inhabitable_drink_list = []
+    save_p = 0
+    drink_number = 0
+    drink_list.each() do |drink|
+      if drink[3] == '可能'
+        inhabitable_drink_list[save_p] = drink_number
+        save_p += 1
       end
-        drinkNumber += 1
+      drink_number += 1
     end
-    return inhabitableDrinkList
+    return inhabitable_drink_list
   end
   
   #自販機の売上金額を出す。
-  def getSales()
+  def viewSales()
     return @sales
   end
 
   #ドリンクを買う。
   #購入した場合、残ったお金を全部払い戻しする。
   #お金が足りない場合、何も行いません。
-  def buyingDrink(drinkNumber)
-    if @drinkList.length > drinkNumber
-      drink = @drinkList[drinkNumber]
+  def buyingDrink(drink_number)
+    if @drink_list.length > drink_number
+      drink = @drink_list[drink_number]
       
-      if drink.getBuyningState(@money) == '可能'
-        drink.updateStock(-1)
-        @money -= drink.getPrice
-        @sales += drink.getPrice
+      if drink[1] <= @money and drink[2] > 0
+        drink[2] -= 1
+        @money -= drink[1]
+        @sales += drink[1]
         #今はputsですが、ここに出る作業が入ります。
-        puts '= buy drink : ' + drink.getName
+        puts '= buy drink : ' + drink[0]
         
         #購入した場合、残ったお金を全部払い戻しする。
-        #refundAllMoney()
+        refundAllMoney()
       end
     end
     #お金が足りない場合、何も行いません。
@@ -140,9 +101,9 @@ class Dihanki
   
   #お金を払い戻し作業は全部そこでする。
   # TODO: 直接呼ぶのは思えません。
-  private def refundMoney(refundMoney)
+  private def refundMoney(refund_money)
     #今はputsですが、ここに出る作業が入ります。
-    puts '= ref money : ' + refundMoney.to_s
+    puts '= ref money : ' + refund_money.to_s
   end
 end
 
@@ -155,24 +116,22 @@ command = ''
 while command != 'end'
   puts '===================='
   puts '[drinkNumber]'
-  drinkNumber = 0
-  inhabitableDrinkList = dihanki.getInhabitableDrinkList()
-  inhabitableCount = 0
-  dihanki.getDrinkList.each() do |drink|
-    if inhabitableCount < inhabitableDrinkList.length
-      if drinkNumber == inhabitableDrinkList[inhabitableCount]
-        print '*'
-        inhabitableCount += 1
-      else
-        print ' '
-      end
+  drink_number = 0
+  inhabitable_drink_list = dihanki.viewInhabitableDrinkList()
+  inhabitable_count = 0
+  dihanki.viewDrinkList().each() do |drink|
+    if drink_number == inhabitable_drink_list[inhabitable_count]
+      print '*'
+      inhabitable_count += 1
+    else
+      print ' '
     end
-    puts '[' + drinkNumber.to_s + '] ' + drink.getBuyningState(dihanki.getMoney()) + ' - ' + drink.getName + '(' + drink.getPrice.to_s + '￥) stock : ' + drink.getStock.to_s + ', メーカー：' + drink.getMake
-    drinkNumber += 1
+    puts '[' + drink_number.to_s + '] ' + drink[3] + ' - ' + drink[0] + '(' + drink[1].to_s + '￥) stock : ' + drink[2].to_s
+    drink_number += 1
   end
   puts '======  data  ======'
-  puts 'dihanki sales = ' + dihanki.getSales.to_s
-  puts 'user money  = ' + dihanki.getMoney.to_s
+  puts 'dihanki sales = ' + dihanki.viewSales().to_s
+  puts 'user money  = ' + dihanki.viewMoney().to_s
   puts '====  commands  ===='
   puts '[number]         : putMoney       (例:10,50,100,500,1000)'
   puts 'get[drinkNumber] : drink buying   (例:get0)'
