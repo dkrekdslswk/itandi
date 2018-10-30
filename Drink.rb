f303f8d2c6327fc5bc004d542dd7dd973033b1a9
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-10-30 20:46:29 +0900

    kadai_3
    클레스를 메인 파일로 부터 분해
    #6 음료수 관리 기능 추가
    - 기존 음료 정보 수정
    - 음료 추가
    - 음료 삭제

---
+++ Drink.rb
@@ -0,0 +1,50 @@
+#Drink Class
+class Drink
+  def initialize(argName, argPrice, argStock)
+    self.setting(argName, argPrice, argStock)
+  end
+  
+  def setting(argName, argPrice, argStock)
+    self.setName(argName)
+    self.setPrice(argPrice)
+    self.setStock(argStock)
+  end
+
+  def getName
+    return @name
+  end
+  
+  def setName(argName)
+    @name = argName.to_s
+  end
+  
+  def getPrice
+    return @price
+  end
+  
+  def setPrice(argPrice)
+    @price = argPrice.to_i
+  end
+  
+  def getStock
+    return @stock
+  end
+  
+  def setStock(argStock)
+    @stock = argStock.to_i
+  end
+  
+  def updateStock(argCount)
+    @stock = @stock + argCount
+  end
+  
+  def getBuyningState(argMoney)
+    if @stock == 0
+      return '無い'
+    elsif @price <= argMoney
+      return '可能'
+    else
+      return '有る'
+    end
+  end
+end
--- main.rb
+++ main.rb
@@ -1,142 +1,9 @@
+require "./Dihanki"
+
 #StringがIntegerかを確認するための設定
 class String
   def integer?
     Integer(self) != nil rescue false
-  end
-end
-
-#Drink Class
-class Drink
-  def initialize(argName, argPrice, argStock)
-    @name = argName
-    @price = argPrice
-    @stock = argStock
-  end
-
-  def getName
-    return @name
-  end
-  
-  def getPrice
-    return @price
-  end
-  
-  def getStock
-    return @stock
-  end
-  
-  def updateStock(argCount)
-    @stock = @stock + argCount
-  end
-  
-  def getBuyningState(argMoney)
-    if @stock == 0
-      return '無い'
-    elsif @price <= argMoney
-      return '可能'
-    else
-      return '有る'
-    end
-  end
-end
-
-#自販機Class
-class Dihanki
-
-  #変数を設定
-  def initialize
-    #お客様が投入したお金
-    @money = 0
-    #自販機の売上金額
-    @sales = 0
-    #ドリンクの情報[ドリンクの番号]
-    @drinkList = [Drink.new('コーラ', 120, 5),
-                  Drink.new('レッドブル', 200, 5),
-                  Drink.new('水', 100, 5)]
-  end
-
-  #return 現在のお客様が投入したお金
-  def getMoney()
-    return @money
-  end
-  
-  #お金の投入
-  #10,50,100,500,1000以外のが投入された場合は、投入金額に加算せず、それをそのまま釣り銭としてユーザに出力する。
-  def putMoney(argMoney)
-    case argMoney
-      when 10
-        @money += argMoney
-      when 50
-        @money += argMoney
-      when 100
-        @money += argMoney
-      when 500
-        @money += argMoney
-      when 1000
-        @money += argMoney
-    else 
-      refundMoney(argMoney)
-    end
-  end
-
-  #自販機のジュースの情報を出す。
-  def getDrinkList()
-    return @drinkList
-  end
-  
-  #購入できるドリンクリストのドリンク番号を出す。
-  def getInhabitableDrinkList()
-    drinkList = self.getDrinkList()
-    inhabitableDrinkList = []
-    saveP = 0
-    drinkNumber = 0
-    drinkList.each() do |drink|
-      if drink.getBuyningState(@money) == '可能'
-        inhabitableDrinkList[saveP] = drinkNumber
-        saveP += 1
-      end
-        drinkNumber += 1
-    end
-    return inhabitableDrinkList
-  end
-  
-  #自販機の売上金額を出す。
-  def getSales()
-    return @sales
-  end
-
-  #ドリンクを買う。
-  #購入した場合、残ったお金を全部払い戻しする。
-  #お金が足りない場合、何も行いません。
-  def buyingDrink(drinkNumber)
-    if @drinkList.length > drinkNumber
-      drink = @drinkList[drinkNumber]
-      
-      if drink.getBuyningState(@money) == '可能'
-        drink.updateStock(-1)
-        @money -= drink.getPrice
-        @sales += drink.getPrice
-        #今はputsですが、ここに出る作業が入ります。
-        puts '= buy drink : ' + drink.getName
-        
-        #購入した場合、残ったお金を全部払い戻しする。
-        #refundAllMoney()
-      end
-    end
-    #お金が足りない場合、何も行いません。
-  end
-  
-  #残ったお金を全部払い戻しする。
-  def refundAllMoney()
-    refundMoney(@money)
-    @money = 0
-  end
-  
-  #お金を払い戻し作業は全部そこでする。
-  # TODO: 直接呼ぶのは思えません。
-  private def refundMoney(refundMoney)
-    #今はputsですが、ここに出る作業が入ります。
-    puts '= ref money : ' + refundMoney.to_s
   end
 end
 
@@ -168,12 +35,15 @@ while command != 'end'
   puts 'dihanki sales = ' + dihanki.getSales.to_s
   puts 'user money  = ' + dihanki.getMoney.to_s
   puts '====  commands  ===='
-  puts '[number]         : putMoney       (例:10,50,100,500,1000)'
-  puts 'get[drinkNumber] : drink buying   (例:get0)'
-  puts 'ref              : refund all money'
-  puts 'end              : end'
+  puts '[number]            : putMoney       (例:10,50,100,500,1000)'
+  puts 'get[drinkNumber]    : drink buying   (例:get0)'
+  puts 'ref                 : refund all money'
+  puts 'update[drinkNumber] : drink update'
+  puts 'insert              : new drink insert'
+  puts 'delete[drinkNumber] : drink delete'
+  puts 'end                 : end'
   puts '===================='
-  print 'command : '
+  print '> '
   command = gets.chomp
 
   puts
@@ -185,6 +55,16 @@ while command != 'end'
   elsif command[0, 3] == 'get'
     if  command[3, command.length].integer?
       dihanki.buyingDrink(command[3, command.length].to_i)
+    end
+  elsif command[0, 6] == 'update'
+    if  command[6, command.length].integer?
+      dihanki.drinkUpdateUI(command[6, command.length].to_i)
+    end
+  elsif command == 'insert'
+    dihanki.drinkInsertUI()
+  elsif command[0, 6] == 'delete'
+    if  command[6, command.length].integer?
+      dihanki.drinkDelete(command[6, command.length].to_i)
     end
   elsif command.integer?
     dihanki.putMoney(command.to_i)
